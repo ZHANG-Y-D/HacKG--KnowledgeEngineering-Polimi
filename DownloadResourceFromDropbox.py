a6f839de6c140de79c4a962bfa79b8bad2938ea4
ZHANG_YD <yuedong.zhang@mail.polimi.it>
2021-07-27 00:25:47 +0200

    Minor improve

--- DownloadResourceFromDropbox.py
+++ DownloadResourceFromDropbox.py
@@ -1,13 +1,18 @@
 import wget
 from bs4 import BeautifulSoup
+#
+# # For participants
+# output_directory = 'Resources/participants'
+# source_path = 'Resources/Dropbox_HTML/participants - Dropbox.html'
 
+# For projects
+output_directory = 'Resources/projects'
+source_path = 'Resources/Dropbox_HTML/projects - Dropbox.html'
 
-f = open("Resources/Dropbox_HTML/participants - Dropbox.html", "r")
+f = open(source_path, "r")
 soup = BeautifulSoup(f.read(), 'html.parser')
 link_form1 = 'https://www.dropbox.com/sh'
-link_form2 = 'participants/'
-output_directory = 'Resources/participants'
-
+link_form2 = '.json'
 
 i = 0
 for link in soup.find_all('a'):
@@ -15,13 +20,6 @@ for link in soup.find_all('a'):
     if link_form1 in link and link_form2 in link:
         wget.download(link, out=output_directory)
         i = i + 1
-        if i > 5000:
+        if i > 10:
             break
         print(link)
-
-
-
-
-
-
-
